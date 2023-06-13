f05973e08aa07098fd4decfaa4056461c04be28c
leobrice <mushibrayson07@gmail.com>
2023-06-13 17:13:57 +0300

    Data part

---
+++ wiki/insert_data.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <form method="post" role="form">
+        <div class="form-group">
+          <input type="text" class="form-control" name="title" placeholder="Title"/>
+        </div>
+        <div class="form-group">
+          <input type="text" name="author" class="form-control" placeholder="Author" id="">
+        </div>
+        <div class="form-group">
+          <input type="text" name="category" class="form-control" placeholder="Category" id="">
+        </div>
+        <div class="form-group">
+          <label> Image </label>
+          <div class="input-group">
+            
+            <span class="input-group-btn">
+              <span class="btn btn-primary btn-file">
+                Browse <input type="file" name="bimgs" multiple>
+              </span>
+             </span>
+            <input type="text" class="form-control" >
+           </div>
+        </div>
+        <div class="form-group">
+          <textarea class="form-control bcontent" name="paragraph01" placeholder="Paragraph" maxlength="700"></textarea>
+        </div>
+        <div class="form-group">
+          <textarea class="form-control bcontent" name="paragraph02" placeholder="Paragraph" maxlength="700"></textarea>
+        </div>
+        <div class="form-group">
+          <textarea class="form-control bcontent" name="paragraph03" placeholder="Paragraph" maxlength="700"></textarea>
+        </div>
+        <div class="form-group"> 
+           <input type="submit" name="Submit" value="Publish" class="btn btn-primary form-control" />
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+    </div>
+</body>
+</html>
